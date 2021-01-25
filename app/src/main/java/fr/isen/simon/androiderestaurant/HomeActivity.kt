8ea7ec60e2584b0e4364bfe86c782871fd338833
TibRib <meta-block@hotmail.fr>
2021-01-25 11:25:44 +0100

    Toasts Bindings

--- app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,29 @@ package fr.isen.simon.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import fr.isen.simon.androiderestaurant.databinding.ActivityHomeBinding
+
+private lateinit var binding: ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        binding.entreesBtn.setOnClickListener{
+            Toast.makeText(applicationContext, "Clicked Entrees", Toast.LENGTH_SHORT).show()
+        }
+        binding.platsBtn.setOnClickListener{
+            Toast.makeText(applicationContext, "Clicked Plats", Toast.LENGTH_SHORT).show()
+        }
+        binding.dessertsBtn.setOnClickListener{
+            Toast.makeText(applicationContext, "Clicked Desserts", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
