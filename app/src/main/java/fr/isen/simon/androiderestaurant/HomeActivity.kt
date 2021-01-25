3229ae58fe6dfe0d5e60713d38e887ecb1d9adba
TibRib <meta-block@hotmail.fr>
2021-01-25 11:09:25 +0100

    Updated VCS

--- app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
@@ -1,21 +1,11 @@
 package fr.isen.simon.androiderestaurant
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-
-private lateinit var binding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityHomeBinding.inflate(LayoutInflater)
-        setContentView(binding.root)
-
-        //Next view
-        binding.buttonEntrance.setOnClickListener{
-            val intent = Intent(this, EntranceActivity::class.java)
-        }
+        setContentView(R.layout.activity_home)
     }
 }
