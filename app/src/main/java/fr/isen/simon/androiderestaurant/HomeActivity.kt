3714a15b8ee95b997f4b1f1f11c8b34d1fd9d76b
TibRib <meta-block@hotmail.fr>
2021-01-26 17:28:29 +0100

    Animations with lottie ! See Details page -> add to cart animation

--- app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/simon/androiderestaurant/HomeActivity.kt
@@ -1,25 +1,17 @@
 package fr.isen.simon.androiderestaurant
 
-import android.content.Context
+import android.R
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.AuthFailureError
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import fr.isen.simon.androiderestaurant.databinding.ActivityHomeBinding
-import java.net.URLEncoder
 
 
 private lateinit var binding: ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
--- app/src/main/java/fr/isen/simon/androiderestaurant/PlatDetailsActivity.kt
+++ app/src/main/java/fr/isen/simon/androiderestaurant/PlatDetailsActivity.kt
@@ -1,13 +1,15 @@
 package fr.isen.simon.androiderestaurant
 
 import android.R
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
-import fr.isen.simon.androiderestaurant.databinding.ActivityCategoryActivityBinding
-import fr.isen.simon.androiderestaurant.databinding.ActivityHomeBinding
 import fr.isen.simon.androiderestaurant.databinding.ActivityPlatDetailsBinding
+
 
 private lateinit var binding: ActivityPlatDetailsBinding
 
@@ -20,12 +22,35 @@ class PlatDetailsActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        val lottieAnimationView = binding.animationView
+        lottieAnimationView.imageAssetsFolder = "images/"
+        binding.cookanimation.imageAssetsFolder = "images/"
+
+        lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {}
+
+            override fun onAnimationEnd(animation: Animator) {
+                val cardAnim = binding.detailsAnimationCard
+                cardAnim.visibility = View.GONE
+                println("Finished !")
+            }
+
+            override fun onAnimationCancel(animation: Animator) { }
+
+            override fun onAnimationRepeat(animation: Animator) {}
+        })
+
         //Get extras:
         val title = intent.getStringExtra("title")
         binding.PlatSingleTitre.text = title
 
         binding.button.setOnClickListener {
             Toast.makeText(applicationContext, "Plat ajouté au panier", Toast.LENGTH_SHORT).show()
+            val mySnackbar = Snackbar.make(view, "Plat ajouté au panier", Snackbar.LENGTH_SHORT)
+
+            val cardAnim = binding.detailsAnimationCard
+            cardAnim.visibility = View.VISIBLE
+
         }
 
         val desc = intent.getStringExtra("description")
